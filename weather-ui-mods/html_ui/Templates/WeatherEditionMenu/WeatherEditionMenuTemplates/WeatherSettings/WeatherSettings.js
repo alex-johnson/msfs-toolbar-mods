82179d8fbe3e4e1f0b18a25207b8acfd5b2d7a3a
Alex Johnson <alexj728@gmail.com>
2021-07-28 08:16:24 -0400

    Updates for SU5

--- weather-ui-mods/html_ui/Templates/WeatherEditionMenu/WeatherEditionMenuTemplates/WeatherSettings/WeatherSettings.js
+++ weather-ui-mods/html_ui/Templates/WeatherEditionMenu/WeatherEditionMenuTemplates/WeatherSettings/WeatherSettings.js
@@ -4,12 +4,199 @@ class WeatherSettingUpdateEvent extends Event {
         this.data = data;
     }
 }
-class WeatherSettingUpdateDateEvent extends Event {
+class DatePickerEvent extends Event {
     constructor(data) {
-        super("dateChanged");
+        super("dateChanged", { bubbles: true });
         this.data = data;
     }
 }
+class DatePickerElement extends ButtonElement {
+    constructor() {
+        super(...arguments);
+        this.m_startYear = 1990;
+        this.m_endYear = 2100;
+        this.m_monthsStrs = ["TT:TIME.JANUARY", "TT:TIME.FEBRUARY", "TT:TIME.MARCH", "TT:TIME.APRIL", "TT:TIME.MAY", "TT:TIME.JUNE", "TT:TIME.JULY", "TT:TIME.AUGUST", "TT:TIME.SEPTEMBER", "TT:TIME.OCTOBER", "TT:TIME.NOVEMBER", "TT:TIME.DECEMBER"];
+        this.onDayChanged = (e) => {
+            this.m_currentDay = e.target.getCurrentValue();
+            this.updateSelected();
+            this.dispatchEvent(new DatePickerEvent(this._data));
+        };
+        this.onMonthChanged = (e) => {
+            this.m_currentMonth = e.target.getCurrentValue();
+            this.createDays();
+            this.m_currentDay = this.m_dayWrapper.querySelector('#Days').getCurrentValue();
+            this.updateSelected();
+            this.dispatchEvent(new DatePickerEvent(this._data));
+        };
+        this.onYearChange = (e) => {
+            let currentYEar = e.target.value;
+            this.m_currentYear = Number(currentYEar);
+            this.createDays();
+            this.updateSelected();
+            this.dispatchEvent(new DatePickerEvent(this._data));
+        };
+        this.updateCursorModeOn = (e) => {
+            this.interactive = false;
+        };
+        this.updateCursorModeOff = (e) => {
+            this.interactive = true;
+        };
+    }
+    get templateID() { return "WeatherDatePickerTemplate"; }
+    connectedCallback() {
+        super.connectedCallback();
+        this.m_dayWrapper = this.querySelector('#dayWrapper');
+        Utils.RemoveAllChildren(this.m_dayWrapper);
+        window.addEventListener("updateExternal:cursorModeOn", this.updateCursorModeOn);
+        window.addEventListener("updateExternal:cursorModeOff", this.updateCursorModeOff);
+    }
+    disconnectedCallback() {
+        window.removeEventListener("updateExternal:cursorModeOn", this.updateCursorModeOn);
+        window.removeEventListener("updateExternal:cursorModeOff", this.updateCursorModeOff);
+    }
+    SetData(_data) {
+        this.SetTimeData(_data.timeData);
+    }
+    GetTimeData() { return this._data; }
+    SetTimeData(_data) {
+        this._data = _data;
+        this.m_currentYear = _data.year;
+        this.m_currentMonth = _data.month - 1;
+        this.m_currentDay = _data.dayInMonth - 1;
+        this._data.dayInMonth = this.m_currentDay + 1;
+        this._data.dayInMonthLocal = this._data.dayInMonth;
+        this._data.month = this.m_currentMonth + 1;
+        this._data.monthLocal = this._data.month;
+        this._data.year = this.m_currentYear;
+        this._data.yearLocal = this._data.year;
+        this.createMonth(_data);
+        this.createDays();
+        this.createYear(_data);
+        this.createDays();
+        this.updateSelected();
+    }
+    CreateButton(_item) {
+        var btn = window.document.createElement("new-list-button");
+        this.FillButton(btn, _item);
+        return btn;
+    }
+    getDefaultChildButton() {
+        return this.m_months;
+    }
+    getKeyNavigationDirection() { return KeyNavigationDirection.KeyNavigation_Vertical; }
+    getAllFocusableChildren() {
+        return [this.m_months, this.m_days, this.m_years];
+    }
+    FillButton(btn, _data) {
+        btn.id = _data.strId;
+        btn.classList.add('listHeader');
+        if (this.classList.contains('date-picker--inline')) {
+            btn.classList.add('inList');
+        }
+        var data = new NewListButtonData();
+        data.strId = _data.strId;
+        data.sTitle = _data.sTitle;
+        data.bLoop = true;
+        data.iDefault = 0;
+        data.daChoices = _data.daChoices;
+        data.daMetadatas = _data.daMetadatas;
+        btn.SetData(data);
+    }
+    createMonth(_data) {
+        let id = "Month";
+        if (this.m_months) {
+            this.m_currentMonth = _data.monthLocal - 1;
+            if (this.m_currentMonth != this.m_months.getCurrentValue())
+                this.m_months.setCurrentValue(this.m_currentMonth);
+        }
+        else {
+            let buttonData = new NewListButtonData();
+            buttonData.strId = id;
+            buttonData.daChoices = this.m_monthsStrs;
+            buttonData.daMetadatas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+            let button = this.CreateButton(buttonData);
+            button.id = id;
+            button.classList.add('linkedToLive');
+            button.title = id;
+            this.m_dayWrapper.appendChild(button);
+            this.m_months = button;
+            this.m_months.addEventListener("OnValidate", this.onMonthChanged);
+        }
+    }
+    createYear(_data) {
+        let id = "Year";
+        if (this.m_years) {
+            this.m_currentYear = _data.yearLocal;
+            if (this.m_currentYear != Number(this.m_months.value))
+                this.m_years.value = this.m_currentYear.toString();
+        }
+        else {
+            let buttonData = new NewListButtonData;
+            buttonData.strId = id;
+            for (let i = this.m_startYear; i <= this.m_endYear; i++) {
+                buttonData.daChoices.push(i.toString());
+            }
+            let button = this.CreateButton(buttonData);
+            button.classList.add('linkedToLive');
+            button.title = id;
+            this.m_years = button;
+            this.m_dayWrapper.appendChild(button);
+            this.m_years.addEventListener("OnValidate", this.onYearChange);
+        }
+    }
+    createDays() {
+        let id = "Days";
+        if (this.m_days) {
+            this.m_currentDay = this._data.dayInMonthLocal - 1;
+            let dayButtonData = new NewListButtonData();
+            dayButtonData.strId = id;
+            let nbDaysInMonth = new Date(this.m_currentYear, this.m_currentMonth + 1, 0).getDate();
+            if (this.m_currentDay > nbDaysInMonth) {
+                this.m_currentDay = nbDaysInMonth;
+            }
+            for (let i = 1; i <= nbDaysInMonth; i++) {
+                dayButtonData.daChoices.push(i.toString());
+            }
+            dayButtonData.iDefault = this.m_currentDay;
+            this.m_days.SetData(dayButtonData);
+        }
+        else {
+            let dayButtonData = new NewListButtonData();
+            dayButtonData.strId = id;
+            let nbDaysInMonth = new Date(this.m_currentYear, this.m_currentMonth + 1, 0).getDate();
+            if (this.m_currentDay > nbDaysInMonth)
+                this.m_currentDay = nbDaysInMonth;
+            for (let i = 1; i <= nbDaysInMonth; i++) {
+                dayButtonData.daChoices.push(i.toString());
+            }
+            let dayButton = this.CreateButton(dayButtonData);
+            dayButton.classList.add('linkedToLive');
+            dayButton.title = id;
+            this.m_dayWrapper.appendChild(dayButton);
+            this.m_days = dayButton;
+            this.m_days.addEventListener("OnValidate", this.onDayChanged);
+            this.sendSizeUpdate();
+        }
+    }
+    updateSelected() {
+        this._data.dayInMonth = this.m_currentDay + 1;
+        this._data.dayInMonthLocal = this._data.dayInMonth;
+        this._data.month = this.m_currentMonth + 1;
+        this._data.monthLocal = this._data.month;
+        this._data.year = this.m_currentYear;
+        this._data.yearLocal = this._data.year;
+        this.m_months.setCurrentValue(this.m_currentMonth);
+        this.m_years.setCurrentValue(this.m_currentYear - this.m_startYear);
+        this.m_days.setCurrentValue(this.m_currentDay);
+    }
+    get year() { return this.m_currentYear; }
+    get month() { return this.m_currentMonth; }
+    get day() { return this.m_currentDay; }
+    set year(year) { this.m_currentYear = year; }
+    set month(month) { this.m_currentMonth = month; }
+    set day(day) { this.m_currentDay = day; }
+}
+window.customElements.define("date-picker", DatePickerElement);
 class WeatherSettingElementData {
 }
 class GroundTemperatureValue extends DataValue {
@@ -61,9 +248,6 @@ class ISAValue extends DataValue {
 class WeatherSettingElement extends TemplateElement {
     constructor() {
         super(...arguments);
-        this.m_monthsStrs = ["TT:TIME.JANUARY", "TT:TIME.FEBRUARY", "TT:TIME.MARCH", "TT:TIME.APRIL", "TT:TIME.MAY", "TT:TIME.JUNE", "TT:TIME.JULY", "TT:TIME.AUGUST", "TT:TIME.SEPTEMBER", "TT:TIME.OCTOBER", "TT:TIME.NOVEMBER", "TT:TIME.DECEMBER"];
-        this.m_startYear = 1990;
-        this.m_endYear = 2100;
         this.onInputGroundSnowDispatchUpdate = (e) => {
             this.m_data.fSnowCover = e.value * 0.01;
             this.dispatchEvent(new WeatherSettingUpdateEvent(this.m_data));
@@ -98,141 +282,9 @@ class WeatherSettingElement extends TemplateElement {
             this.m_data.bIsAltitudeAMGL = JSON.parse(this.m_input_altitude_calculation_mode.metadata);
             this.dispatchEvent(new WeatherSettingUpdateEvent(this.m_data));
         };
-        this.onDayChanged = (e) => {
-            this.m_currentDay = e.target.getCurrentValue();
-            this.m_time_data.dayInMonth = this.m_currentDay + 1;
-            this.updateSelected();
-            this.dispatchEvent(new WeatherSettingUpdateDateEvent(this.m_time_data));
-        };
-        this.onMonthChanged = (e) => {
-            this.m_currentMonth = e.target.getCurrentValue();
-            this.createDays();
-            this.m_currentDay = this.m_dayWrapper.querySelector('#Days').getCurrentValue();
-            this.m_time_data.dayInMonth = this.m_currentDay + 1;
-            this.m_time_data.month = this.m_currentMonth + 1;
-            this.updateSelected();
-            this.dispatchEvent(new WeatherSettingUpdateDateEvent(this.m_time_data));
-        };
-        this.onYearChanged = (e) => {
-            let currentYEar = e.target.value;
-            this.m_currentYear = Number(currentYEar);
-            this.createDays();
-            this.m_time_data.dayInMonth = this.m_currentDay + 1;
-            this.m_time_data.year = this.m_currentYear;
-            this.updateSelected();
-            this.dispatchEvent(new WeatherSettingUpdateDateEvent(this.m_time_data));
-        };
-        this.createMonth = () => {
-            let id = "Month";
-            this.m_currentMonth = this.m_time_data.month - 1;
-            if (this.m_months) {
-                if (this.m_currentMonth != this.m_months.getCurrentValue())
-                    this.m_months.setCurrentValue(this.m_currentMonth);
-            }
-            else {
-                let buttonData = new NewListButtonData;
-                buttonData.strId = id;
-                buttonData.daChoices = this.m_monthsStrs;
-                buttonData.daMetadatas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
-                buttonData.iDefault = this.m_currentMonth + 1;
-                let button = this.createButton(buttonData);
-                button.id = id;
-                button.title = id;
-                button.classList.add('linkedToLive');
-                this.m_months = button;
-                this.m_dayWrapper.appendChild(button);
-                button.setAttribute("default-button", "");
-                this.m_months.addEventListener("OnValidate", this.onMonthChanged.bind(this));
-            }
-        };
-        this.createDays = () => {
-            let id = "Days";
-            this.m_currentDay = this.m_time_data.dayInMonth - 1;
-            if (this.m_days) {
-                let daysButton = this.m_dayWrapper.querySelector('#' + id);
-                let dayButtonData = new NewListButtonData;
-                dayButtonData.strId = id;
-                let nbDaysInMonth = new Date(this.m_currentYear, this.m_currentMonth + 1, 0).getDate();
-                if (this.m_currentDay > nbDaysInMonth) {
-                    this.m_currentDay = nbDaysInMonth;
-                }
-                for (let i = 1; i <= nbDaysInMonth; i++) {
-                    dayButtonData.daChoices.push(i.toString());
-                }
-                dayButtonData.iDefault = this.m_currentDay;
-                daysButton.SetData(dayButtonData);
-            }
-            else {
-                let dayButtonData = new NewListButtonData;
-                dayButtonData.strId = id;
-                let nbDaysInMonth = new Date(this.m_currentYear, this.m_currentMonth + 1, 0).getDate();
-                if (this.m_currentDay > nbDaysInMonth)
-                    this.m_currentDay = nbDaysInMonth;
-                for (let i = 1; i <= nbDaysInMonth; i++) {
-                    dayButtonData.daChoices.push(i.toString());
-                }
-                let daysButton = this.createButton(dayButtonData);
-                daysButton.classList.add('linkedToLive');
-                daysButton.title = id;
-                daysButton.value = (this.m_currentDay + 1).toString();
-                daysButton.setAttribute("default-button", "");
-                this.m_dayWrapper.appendChild(daysButton);
-                this.m_days = daysButton;
-                this.m_days.addEventListener("OnValidate", this.onDayChanged);
-            }
-        };
-        this.createYears = () => {
-            let id = "Year";
-            this.m_currentYear = this.m_time_data.year;
-            if (this.m_years) {
-                if (this.m_currentYear != Number(this.m_months.value))
-                    this.m_years.value = this.m_currentYear.toString();
-            }
-            else {
-                let buttonData = new NewListButtonData;
-                buttonData.strId = id;
-                for (let i = this.m_startYear; i <= this.m_endYear; i++) {
-                    buttonData.daChoices.push(i.toString());
-                }
-                buttonData.iDefault = this.m_currentYear;
-                let button = this.createButton(buttonData);
-                button.classList.add('linkedToLive');
-                button.title = id;
-                button.setAttribute("default-button", "");
-                this.m_dayWrapper.appendChild(button);
-                this.m_years = button;
-                this.m_years.addEventListener("OnValidate", this.onYearChanged.bind(this));
-            }
-        };
-        this.createButton = (_item) => {
-            var btn = window.document.createElement("new-list-button");
-            btn.classList.add('listHeader');
-            this.fillButton(btn, _item);
-            return btn;
-        };
-        this.fillButton = (btn, _data) => {
-            var nz = new Name_Z(_data.strId);
-            btn.id = _data.strId;
-            var data = new NewListButtonData();
-            data.strId = _data.strId;
-            data.sTitle = _data.sTitle;
-            data.bLoop = true;
-            data.iDefault = 0;
-            data.daChoices = _data.daChoices;
-            data.daMetadatas = _data.daMetadatas;
-            btn.SetData(data);
-        };
-        this.updateSelected = () => {
-            this.m_months.setCurrentValue(this.m_currentMonth);
-            this.m_years.setCurrentValue(this.m_currentYear - this.m_startYear);
-            this.m_days.setCurrentValue(this.m_currentDay);
-        };
     }
     get templateID() { return "WeatherSettingsTemplate"; }
     ;
-    get year() { return this.m_currentYear; }
-    get month() { return this.m_currentMonth; }
-    get day() { return this.m_currentDay; }
     connectedCallback() {
         super.connectedCallback();
         UIWeatherConfig.F_MAX_PRECIPITATION = 300.0;
@@ -273,8 +325,6 @@ class WeatherSettingElement extends TemplateElement {
         this.m_input_isa.addEventListener('update', this.onInputISAUpdate);
         this.m_input_altitude_calculation_mode = this.querySelector('new-list-button.altitudeCalculation');
         this.m_input_altitude_calculation_mode.addEventListener('OnValidate', this.onAltitudeCalcultationModeChanged);
-        this.m_dayWrapper = this.querySelector('#dayWrapper');
-        Utils.RemoveAllChildren(this.m_dayWrapper);
         this.querySelector("virtual-scroll").updateSizes();
     }
     setData(data) {
@@ -308,16 +358,6 @@ class WeatherSettingElement extends TemplateElement {
             this.m_input_altitude_calculation_mode.setCurrentValue(0);
         }
         this.m_input_aerosoldensity.sendSizeUpdate();
-    }
-    setTimeData(data) {
-        this.m_time_data = data;
-        if (!this.isConnected)
-            return;
-        this.createMonth();
-        this.createDays();
-        this.createYears();
-        this.updateSelected();
-        this.m_dayWrapper.parentElement.classList.remove('hide');
     }
 }
 window.customElements.define("weather-settings", WeatherSettingElement);
